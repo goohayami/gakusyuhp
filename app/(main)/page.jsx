a9d5bdd761c11c75752940d741edb0377fe49a9f
hayami <goohayami@gmail.com>
2024-09-16 20:38:46 +0900

    conanYoutube

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -2,7 +2,19 @@ const Home = () => {
   return (
     <div>
       <div className=" rounded-full bg-gray-800 ">
-        <h1 className=" text-white flex justify-center mt-4 mx-12 py-2">はじめに</h1>
+        <h1 className=" text-white flex justify-center mt-4 mx-12 py-2">
+          はじめに
+        </h1>
+      </div>
+      <div className="px-16 pt-8 text-2xl">
+        <h1>
+          学習発表会の練習につかえたらいいなと思って作りました。
+          <br />
+          がくふや、お手本を見ながら、ひとりでも練習をすすめることが、できたらよいなと思っています。
+          <br />
+          まだ、作りかけですが、つかえるところがあったら、どんどんつかってくださいね。
+          <br />
+        </h1>
       </div>
     </div>
   );
