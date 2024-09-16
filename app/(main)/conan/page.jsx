6e5e71992b2f201c17b2140091455be63d397e34
hayami <goohayami@gmail.com>
2024-09-16 17:03:33 +0900

    ok

--- app/(main)/conan/page.jsx
+++ app/(main)/conan/page.jsx
@@ -1,7 +1,15 @@
+"use client";
+// import a from "../components/video.mp4";
+
 const page = () => {
   return (
     <div>
       <h1>ここはコナン</h1>
+      <video width="320" height="240" controls preload="none">
+        <source src="./test.mp4" type="video/mp4" />
+        <track src="./test.mp4" kind="subtitles" srcLang="en" label="English" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
