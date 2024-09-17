db8cf941022fbc7b0c209d56ec6879c400548083
hayami <goohayami@gmail.com>
2024-09-17 18:33:57 +0900

    w-60

--- app/(main)/components/SideMenu/SideMenu.jsx
+++ app/(main)/components/SideMenu/SideMenu.jsx
@@ -1,7 +1,7 @@
 import NavList from "../NavList/NavList";
 const SideMenu = () => {
   return (
-    <div className="w-60 pt-8 bg-gray-800 text-white">
+    <div className="w-80 pt-8 bg-gray-800 text-white">
       <div>
         <h1 className="p-3 tx-4 text-xl font-bold">学習発表会</h1>
         <NavList />
