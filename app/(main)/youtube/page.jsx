6c3ca4bda30188d39eac2b7411f56a16ee371c04
hayami <goohayami@gmail.com>
2024-09-17 19:28:26 +0900

    youtube

--- app/(main)/youtube/page.jsx
+++ app/(main)/youtube/page.jsx
@@ -1,6 +1,9 @@
 import { YouTubeEmbed } from "@next/third-parties/google";
+import Image from "next/image";
 
 const youtube = () => {
+  const imagePath_1 = "/conan_1.jpg";
+  const imagePath_2 = "/conan_2.jpg";
   return (
     <div>
       <h1 className="rounded-full bg-gray-800 text-white flex justify-center my-4 py-2">
@@ -8,12 +11,17 @@ const youtube = () => {
       </h1>
       <div>
         <YouTubeEmbed
-          className="flex justify-center"
+          className="flex justify-center "
           videoid="lFnU_xxEgjQ"
           width={700}
-          height={700}
+          height={450}
           params="controls=1"
         />
+        <p className="text-gray-800 font-bold flex justify-center mb-2">
+          けんばんハーモニカ　がくふ
+        </p>
+        <Image src={imagePath_1} width={700} height={200} alt={"楽譜"} />
+        <Image src={imagePath_2} width={700} height={200} alt={"楽譜"} />
       </div>
     </div>
   );
