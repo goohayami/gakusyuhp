c7f73c5e2b36a2246c09479ea9200a5c081824d6
hayami <goohayami@gmail.com>
2024-09-18 20:46:04 +0900

    コナンけんばん

--- app/(main)/kenban-a/page.jsx
+++ app/(main)/kenban-a/page.jsx
@@ -8,10 +8,23 @@ const merry = () => {
       <h1 className="rounded-full bg-gray-800 text-white flex justify-center my-4 py-2">
         名探偵コナン　メインテーマ　けんばんⒶⒷⒸ練習用
       </h1>
+      <p className="text-gray-800 font-bold flex justify-center mb-2">
+        がくふデモ
+      </p>
       <div>
         <YouTubeEmbed
           className="flex justify-center"
           videoid="e8QF6hSL4bs"
+          width={700}
+          height={450}
+          params="controls=1"
+        />
+        <p className="text-gray-800 font-bold flex justify-center mb-2">
+          けんばんデモ
+        </p>
+        <YouTubeEmbed
+          className="flex justify-center"
+          videoid="J6m26a0QVm0"
           width={700}
           height={450}
           params="controls=1"
