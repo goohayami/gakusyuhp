74349d8fe4ac4b7ff1d1b2944173eb6f8e390a34
hayami <goohayami@gmail.com>
2024-09-18 19:30:42 +0900

    メリーさん

--- app/(main)/components/NavList/NvItem/NavItem.jsx
+++ app/(main)/components/NavList/NvItem/NavItem.jsx
@@ -9,7 +9,9 @@ const NavItem = ({ label, link, icon }) => {
       <Link
         href={link}
         className={`flex p-4 items-center w-full hover:bg-gray-700 font-midium  whitespace-nowrap ${
-          pathname === link ? "bg-gray-600 border-r-4 border-r-yellow-200" : ""
+          pathname === link
+            ? "bg-gray-700 border-r-4 border-r-red-600 text-white"
+            : ""
         }`}
       >
         <div className="mr-2">{icon}</div>
--- app/(main)/components/SideMenu/SideMenu.jsx
+++ app/(main)/components/SideMenu/SideMenu.jsx
@@ -1,9 +1,9 @@
 import NavList from "../NavList/NavList";
 const SideMenu = () => {
   return (
-    <div className="w-80 pt-8 bg-gray-800 text-white">
+    <div className="w-80 pt-8 bg-gray-800 text-gray-400">
       <div>
-        <h1 className="p-3 tx-4 text-xl font-bold">学習発表会</h1>
+        <h1 className="p-3 tx-4 text-xl font-bold text-white">学習発表会</h1>
         <NavList />
       </div>
     </div>
