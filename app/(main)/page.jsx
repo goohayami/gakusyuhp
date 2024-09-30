1afcf95cce9381cdc3c0e5b87b6b43a942897bf1
goohayami <goohayami.gmail.com>
2024-09-30 21:11:32 +0900

    conan

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 const Home = () => {
-  const imagePath_1 = "/kuteku.jpg";
+  const imagePath_1 = "/cul.jpg";
   return (
     <div>
       <div className=" rounded-full bg-gray-800 ">
@@ -8,19 +8,12 @@ const Home = () => {
           はじめに
         </h1>
       </div>
-
-      <div className="px-16 pt-8 text-2xl">
-        <Image src={imagePath_1} width={250} height={300} alt={"楽譜"} />
-        <h1>
-          ３年生のみなさん、こんにちは!
-          <br />
-          学習発表会が11月にあるんだってね。
-          <br />
-          みんなのために、ぼくが練習用のウェブサイトを作ったよ。
-          <br />
-          みんなのえんそうが、楽しみだな！
-        </h1>
+      <div className="px-16 pt-8 text-xl mt-0 mb-0">さぁ、10月ですね学習発表会の練習をがんばりましょう！</div>
+      <div className="px-16 pt-8 text-2xl pt-1">
+        <Image src={imagePath_1} width={480} height={300} alt={"楽譜"} />
+       
       </div>
+    
     </div>
   );
 };
